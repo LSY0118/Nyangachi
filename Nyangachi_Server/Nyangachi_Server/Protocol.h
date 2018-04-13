a80c5dc25a1cf65c45478519107ad1e50a2270fe
GuMinYoung <rnalsdud1014@naver.com>
2018-04-13 21:59:50 +0900

    NetworkManager.cpp 작성 중
    iocp

--- Nyangachi_Server/Nyangachi_Server/Protocol.h
+++ Nyangachi_Server/Nyangachi_Server/Protocol.h
@@ -3,6 +3,11 @@
 
 #define		MAX_PACKET_SIZE			256
 #define		SERVER_PORT				9000
+#define		RESERVE_ID_CNT			1000
+#define		PAK_SYNC				0
+#define		PAK_ID					1
+#define		PAK_REG					2
+#define		PAK_RMV					3
 
 #pragma pack(push, 1)
 
